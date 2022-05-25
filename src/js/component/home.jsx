bdcb13e0355361f006ab34e7e1b609466a575ec2
alonsogomezo <luisalonsogo95@gmail.com>
2022-05-25 00:50:11 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const people = [
+		{ id: 1, name: "Peter", age: 32, country: "MX" },
+		{ id: 2, name: "Maria", age: 15, country: "CR" },
+		{ id: 3, name: "Paula", age: 53, country: "CO" },
+		{ id: 4, name: "Cristina", age: 48, country: "CR" },
+		{ id: 5, name: "James", age: 14, country: "PE" },
+		{ id: 6, name: "Clara", age: 19, country: "CO" },
+		{ id: 7, name: "Stuart", age: 29, country: "CR" },
+		{ id: 8, name: "Miriam", age: 11, country: "UY" },
+	];
+
+	const [gente, setGente] = useState(people);
+
+	const myFunc = () => {
+		let newGente = gente.filter(
+			(item) => item.country === "CO"
+		); /*newGente es un 
+		arreglo que almacena todos los objetos con country CO de gente*/
+		setGente(newGente);
+	};
+
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">Hola</h1>
+			{gente.map((item, index) => (
+				<div key={index}>
+					<p>{`${item.name} es de ${item.country}`}</p>
+				</div>
+			))}
+			<button onClick={() => myFunc()}>Solo los de CO</button>
 		</div>
 	);
 };
